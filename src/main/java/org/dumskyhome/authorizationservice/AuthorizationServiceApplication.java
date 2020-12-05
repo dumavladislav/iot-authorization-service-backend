dd69b5e9b44660a460133ec14cb2520a5e3f7940
dumavladislav <vladislav.v.duma@gmail.com>
2020-12-06 00:33:45 +0300

    Database schema parameterized

--- src/main/java/org/dumskyhome/authorizationservice/AuthorizationServiceApplication.java
+++ src/main/java/org/dumskyhome/authorizationservice/AuthorizationServiceApplication.java
@@ -4,9 +4,12 @@ import org.dumskyhome.authorizationservice.Mqtt.MqttAgent;
 import org.dumskyhome.authorizationservice.service.AuthorizationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
